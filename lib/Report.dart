588dd4942af43041ab5588821f4e1b7717da9ebe
Dhruv Choudhary <dr3515@srmist.edu.in>
2023-06-26 12:44:37 +0530

    Added Backend for Report

--- lib/Report.dart
+++ lib/Report.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ReportIncidentScreen extends StatefulWidget {
   @override
@@ -10,14 +11,21 @@ class _ReportIncidentScreenState extends State<ReportIncidentScreen> {
   String complaint = '';
   String selectedTag = '';
 
-  List<String> tags = ['Account Issues', 'Fake News', 'Summary Problem', 'User Problem', 'Explicit Content'];
+  List<String> tags = [
+    'Account Issues',
+    'Fake News',
+    'Summary Problem',
+    'User Problem',
+    'Explicit Content'
+  ];
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Report Incident'),
-
         backgroundColor: const Color(0xFF023436),
       ),
       body: Padding(
@@ -59,46 +67,115 @@ class _ReportIncidentScreenState extends State<ReportIncidentScreen> {
               ),
             ),
             const SizedBox(height: 16.0),
-        const Text(
+            const Text(
               'Tags',
               style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8.0),
-          Center(
-            child: Wrap(
-              spacing:20.0,
-              children: tags.map((tag) {
-                return ChoiceChip(
-                  label: Text(tag),
-                  selected: selectedTag == tag,
-                  onSelected: (selected) {
-                    setState(() {
-                      selectedTag = selected ? tag : '';
-                    });
-                  },
-                );
-              }).toList(),
+            Center(
+              child: Wrap(
+                spacing: 20.0,
+                children: tags.map((tag) {
+                  return ChoiceChip(
+                    label: Text(tag),
+                    selected: selectedTag == tag,
+                    onSelected: (selected) {
+                      setState(() {
+                        selectedTag = selected ? tag : '';
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
             ),
-          ),
             const SizedBox(height: 16.0),
-
             Center(
               child: ElevatedButton(
                 onPressed: () {
-                  // Handle submit button action
+                  _submitReport();
                 },
                 style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white, backgroundColor: const Color(0xFF023436), // Set text color to white
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0), // Increase horizontal padding
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xFF023436),
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
                 ),
                 child: const Text('Submit'),
               ),
-            )
-
-
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void _submitReport() {
+    if (title.isNotEmpty && complaint.isNotEmpty && selectedTag.isNotEmpty) {
+      _firestore.collection('incidents').add({
+        'title': title,
+        'complaint': complaint,
+        'tag': selectedTag,
+        'timestamp': FieldValue.serverTimestamp(),
+      }).then((_) {
+        // Reset the form after successful submission
+        setState(() {
+          title = '';
+          complaint = '';
+          selectedTag = '';
+        });
+
+        showDialog(
+          context: context,
+          builder: (context) =>
+              AlertDialog(
+                title: const Text('Success'),
+                content: const Text('Incident report submitted successfully.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              ),
+        );
+      }).catchError((error) {
+        showDialog(
+          context: context,
+          builder: (context) =>
+              AlertDialog(
+                title: const Text('Error'),
+                content: Text(
+                    'An error occurred while submitting the report: $error'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              ),
+        );
+      });
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) =>
+            AlertDialog(
+              title: const Text('Error'),
+              content: const Text('Please fill in all the required fields.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+      );
+    }
   }
 }
